c4071572c886753a8c178bf8f816415d7c3a938c
Mousa Adolai <mousa.adolai@gmail.com>
2021-10-22 15:38:47 -0400

    Added cucumber dependencies, created file navigation feature file, and created file page class.

---
+++ src/test/java/step_definitions/FileNavigationStepDefs.java
@@ -0,0 +1,23 @@
+package step_definitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class FileNavigationStepDefs {
+
+    @Given("user logs in")
+    public void userLogsIn() {
+    }
+
+
+    @When("user clicks file module")
+    public void userClicksFileModule() {
+
+    }
+
+    @Then("verify user is on file page")
+    public void verifyUserIsOnFilePage() {
+    }
+
+}
